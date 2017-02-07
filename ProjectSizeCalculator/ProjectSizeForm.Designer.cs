be81f21810e691c97c5ad04f3aff119b8ccc2b33
Kookpot <Koen.plasmans@telenet.be>
2017-02-07 17:27:14 +0100

    Initial commit

---
+++ ProjectSizeCalculator/ProjectSizeForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ProjectSizeCalculator
+{
+    partial class ProjectSizeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 28);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(379, 294);
+            this.treeView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(379, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.fileToolStripMenuItem.Text = "&Project";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItemClick);
+            // 
+            // ProjectSizeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 322);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ProjectSizeForm";
+            this.Text = "Project Size Calculator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ ProjectSizeCalculator/ProjectSizeForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProjectSizeCalculator
+{
+    /// <summary>
+    /// form to show size of project
+    /// </summary>
+    public partial class ProjectSizeForm : Form
+    {
+        #region Members
+
+        private readonly List<string> _list;
+
+        #endregion
+
+        #region Constructors
+
+        public ProjectSizeForm()
+        {
+            InitializeComponent();
+            _list = new List<string> {".cs", ".aspx", ".js", ".css", ".asmx", ".htm", ".ascx", ".asax"};
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void OpenToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var count = 0;
+                treeView1.Nodes.Add(CalculateFolder(folderBrowserDialog1.SelectedPath, ref count));
+            }
+        }
+
+        private TreeNode CalculateFolder(string folder, ref int count)
+        {
+            var treeNode = new TreeNode();
+            var info = new DirectoryInfo(folder);
+            int[] intCounter = {0};
+            treeNode.Nodes.AddRange(Directory.GetDirectories(folder).Select(strInnerFolder => CalculateFolder(strInnerFolder, ref intCounter[0]))
+                .Where(objNode => objNode.Nodes.Count > 0).ToArray());
+
+            for (var index = 0; index < Directory.GetFiles(folder).Length; index++)
+            {
+                var innerFile = Directory.GetFiles(folder)[index];
+                var fileInfo = new FileInfo(innerFile);
+                if (_list.Contains(fileInfo.Extension))
+                {
+                    treeNode.Nodes.Add(CalculateFile(innerFile, ref intCounter[0]));
+                }
+            }
+            treeNode.Tag = count;
+            treeNode.Text = info.Name + @" : " + intCounter[0] + @" lines";
+            treeNode.Checked = true;
+            count += intCounter[0];
+            return treeNode;
+        }
+
+        private static TreeNode CalculateFile(string file, ref int count)
+        {
+            var treeNode = new TreeNode();
+            var info = new FileInfo(file);
+            var stream = new StreamReader(file);
+            var counter = 0;
+            while (!stream.EndOfStream)
+            {
+                stream.ReadLine();
+                counter++;
+            }
+            stream.Close();
+            treeNode.Tag = counter;
+            treeNode.Text = info.Name + @" : " + counter + @" lines";
+            treeNode.Checked = true;
+            count += counter;
+            return treeNode;
+        }
+
+        #endregion
+    }
+}
